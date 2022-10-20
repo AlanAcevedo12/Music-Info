e547079c5165a1c839fe65ee46a774eec0ad96f5
AlanAcevedo12 <alanacevedo2001@hotmail.com>
2022-10-19 22:50:00 -0300

    Feat: WIP register validations

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import styles from "./Register.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import {register} from "../../redux/actions/actions"
+import { register } from "../../redux/actions/actions"
 
 function Register() {
     const dispatch = useDispatch();
     const registerError = useSelector(state => state.registerError);
     const [input, setInput] = useState({ name: "", surname: "", email: "", password: "", username: "", age: "" });
+    const [errors, setErrors] = useState({ name: "", surname: "", email: "", password: "", username: "", age: "" });
 
     function onChangeHandler(e) {
-        setInput({ ...input, [e.target.name]: e.target.value })
+        if (e.target.name === "age") if (/[^0-9]/.test(e.target.value)) return;
+        setInput({ ...input, [e.target.name]: e.target.value });
+        setErrors(validation({ name: e.target.name, value: e.target.value }, input, errors));
     }
 
     function submitHandler(e) {
@@ -25,11 +28,13 @@ function Register() {
             <form id={styles.form} onSubmit={submitHandler}>
                 <div className={styles.inputContainer}>
                     <label className={styles.inputLabel}>Correo electrónico</label>
-                    <input className={styles.input} type="text" name="email" value={input.email} onChange={onChangeHandler} />
+                    <input className={errors.email.length ? styles.errorInput : styles.input}
+                        type="text" name="email" value={input.email} onChange={onChangeHandler} />
                 </div>
                 <div className={styles.inputContainer}>
                     <label className={styles.inputLabel}>Nombre</label>
-                    <input className={styles.input} type="text" name="name" value={input.name} onChange={onChangeHandler} />
+                    <input className={errors.name.length ? styles.errorInput : styles.input} 
+                    type="text" name="name" value={input.name} onChange={onChangeHandler} />
                 </div>
                 <div className={styles.inputContainer}>
                     <label className={styles.inputLabel}>Apellido</label>
@@ -45,7 +50,8 @@ function Register() {
                 </div>
                 <div className={styles.inputContainer}>
                     <label className={styles.inputLabel}>Edad</label>
-                    <input className={styles.input} type="text" name="age" value={input.age} onChange={onChangeHandler} />
+                    <input className={styles.input} type="text" name="age" maxLength="2"
+                        value={input.age} onChange={onChangeHandler} />
                 </div>
                 <button id={styles.submitButton} type="submit">REGISTRARSE</button>
             </form>
@@ -54,3 +60,22 @@ function Register() {
 }
 
 export default Register;
+
+export function validation(input, globalInput, errors) {
+    if (errors[input.name]) errors[input.name] = "";
+
+    switch (input.name) {
+        case "email":
+            if (/(.+)@(.+){2,}\.(.+){2,}/.test(input.value))
+                errors.email = "";
+            else
+                errors.email = "Formato inválido";
+        break;
+        case "name":
+            if(/[0-9]/.test(input.value)) errors.name = "Nombre inválido";
+            if(/[@$?¡\-_]/.test(input.value)) errors.name = "Nombre inválido";
+        break;
+    }
+
+    return errors;
+}
