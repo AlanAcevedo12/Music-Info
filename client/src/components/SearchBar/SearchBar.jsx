49a111bcb43d6726f21223d97c0a5223dc7542b9
AlanAcevedo12 <alanacevedo2001@hotmail.com>
2022-10-31 19:26:19 -0300

    Feat: LogOut button works

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAlbum, getArtist, getTrack, logout } from "../../redux/actions/actions";
 import styles from "./SearchBar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function SearchBar() {
     const dispatch = useDispatch();
     const [input, setInput] = useState("");
     const user = useSelector(state => state.user);
+    const navigate = useNavigate();
 
     function onSubmitHandler(e) {
         e.preventDefault();
@@ -20,7 +21,11 @@ function SearchBar() {
         setInput(e.target.value);
     }
 
-    async function logoutHandler(){
+    function goToProfile(){
+        navigate("/profile");
+    }
+
+    async function logoutHandler() {
         await dispatch(logout());
         window.location.reload();
     }
@@ -54,9 +59,9 @@ function SearchBar() {
                     :
                     <>
                         <div id={styles.userContainer}>
-                            {/* <button id={styles.userButton}>
-                            <img src="https://e-cdns-images.dzcdn.net/images/user//32x32-000000-80-0-0.jpg" />
-                        </button> */}
+                            <button id={styles.userButton} onClick={goToProfile}>
+                                <img src="https://e-cdns-images.dzcdn.net/images/user//32x32-000000-80-0-0.jpg" />
+                            </button>
                             <h1>{user.name}</h1>
                         </div>
                         <button onClick={logoutHandler}>EXIT</button>
