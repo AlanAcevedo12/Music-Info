0ede28d6d0e3c82889dc43d383e91cc9b14f268b
AlanAcevedo12 <alanacevedo2001@hotmail.com>
2022-11-03 20:48:22 -0300

    Fix: Play button on album now has the correct stetic

--- client/src/components/AlbumCard/AlbumCard.jsx
+++ client/src/components/AlbumCard/AlbumCard.jsx
@@ -4,9 +4,21 @@ function AlbumCard({ item, getAlbum }) {
 
     return (
         <div id={styles.albumContainer}>
-            <div id={styles.imageConatiner}>
-                <img src={item.cover_medium} id={styles.image} />
+            <div id={styles.albumImage}>
+                <div id={styles.imageConatiner}>
+                    <div id={styles.filter}></div>
+                    <img src={item.cover_medium} id={styles.image} />
+                </div>
+                <div id={styles.buttonContainer}>
+                    <button id={styles.playButton} onClick={() => getAlbum(item)}>
+                        <svg focusable="false" height="1em" role="img" width="1em" viewBox="0 0 12 12" aria-hidden="true">
+                            <path fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M2.5.5v11l9-5.5z">
+                            </path>
+                        </svg>
+                    </button>
+                </div>
             </div>
+
             <div id={styles.infoContainer}>
                 <div id={styles.info}>
                     <span id={styles.name}>{item.title}</span>
@@ -15,7 +27,6 @@ function AlbumCard({ item, getAlbum }) {
                     <span id={styles.fans}>{"de " + item.artist.name}</span>
                 </div>
                 {item.explicit_lyrics && <div id={styles.explicit}>EXPLICIT</div>}
-                <button onClick={() => getAlbum(item)}>PLAY</button>
             </div>
         </div>
     )
