86d64742ee1a5ea5de86843c40b5b6aae2ebfa10
AlanAcevedo12 <alanacevedo2001@hotmail.com>
2022-11-14 23:19:39 -0300

    Fix: now throws a status 400 and error msg when trying to add a track that is in the favlist

--- api/src/controllers/removeFavorite.js
+++ api/src/controllers/removeFavorite.js
@@ -3,19 +3,17 @@ const axios = require("axios");
 const router = Router();
 const { User } = require("../db.js");
 
-router.put("/addFav", async (req, res) => {
+router.put("/removeFav", async (req, res) => {
     const { trackId, userId } = req.body;
     if (!trackId) res.status(400).send("Datos incorrectos");
     try {
         let user = await User.findByPk(userId);
-        if(user.favoriteTracks.indexOf(trackId) === -1){
-            user.favoriteTracks.push(trackId);
-            let updated = await User.update({ "favoriteTracks": user.favoriteTracks },{where: {id: userId}});
-            res.status(200).send(updated);
-        }else{
-            res.status(400).send({msg: "La canción ya se encuentra en Favoritos"})
-        }
+        user.favoriteTracks.push(trackId);
+        let updated = await User.update({ "favoriteTracks": user.favoriteTracks },{where: {id: userId}});
 
+        // await updated.save();
+
+        res.status(200).send(updated);
     } catch (e) {
         res.status(400).send({ msg: "Error desconocido", e });
     }
